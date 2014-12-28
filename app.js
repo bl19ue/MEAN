209c8023b24f7778fcb780efb158639fd7e6cd7e
Sumit Valecha <svalecha91@gmail.com>
2014-12-27 17:56:59 -0800

    2.0.0

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 angular.module('myNews', [])
 	.controller('NewsCtrl', ['$scope', function($scope){
 		$scope.say_hello = "Hey!!!!!!";
+		$scope.news = [										//News array
+			'News 1',
+			'News 2',
+			'News 3',
+			'News 4',
+			'News 5'
+		];
 	}
 ]);
